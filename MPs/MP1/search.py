d9db6a4b5ff1bc782de5e653420c2ae5ca134b44
hujiadfr <jiarun126@hotmail.com>
2023-03-09 20:01:02 +0800

    add bfs dfs astar

--- MPs/MP1/search.py
+++ MPs/MP1/search.py
@@ -13,7 +13,9 @@ This is the main entry point for MP1. You should only modify code
 within this file -- the unrevised staff files will be used for all other
 files and classes when code is run, so be careful to not modify anything else.
 """
-
+import maze
+import queue
+import sys
 
 # Search should return the path and the number of states explored.
 # The path should be a list of tuples in the form (row, col) that correspond
@@ -32,15 +34,53 @@ def search(maze, searchMethod):
 
 
 def bfs(maze):
-    # TODO: Write your code here
-    # return path, num_states_explored
-    return [], 0
+   queue = []# frontier, fifo queue
+   visited = set()# set of points that has been visited
+   queue.append([maze.getStart()]) # start from begin point
+   while queue:
+#         current path & point
+       curr_path = queue.pop(0)
+       curr_row, curr_col = curr_path[-1]
+       curr_point = (curr_row, curr_col)
+        #  if find the end
+       if maze.isObjective(curr_row, curr_col):
+           return curr_path, len(visited)
+#          this point has been visited
+       if curr_point in visited:
+           continue
+       visited.add(curr_point)
+        #  add new point into new path
+       for new_point in maze.getNeighbors(curr_row, curr_col):
+           if new_point not in visited:
+               queue.append(curr_path + [new_point])
+   return [], 0
+
 
 
 def dfs(maze):
     # TODO: Write your code here
     # return path, num_states_explored
-    return [], 0
+    path_stack = []
+    visited = []
+    num_states_explored = 0
+    path_stack.append([maze.getStart()])
+    while path_stack:
+        path = path_stack.pop()
+        cur_loc = path[-1]
+        if cur_loc in visited:
+            continue
+        num_states_explored += 1
+        visited.append(cur_loc)
+        cur_row, cur_col = cur_loc
+        if maze.isObjective(cur_row, cur_col):
+            return path, num_states_explored
+        for neighbor in maze.getNeighbors(cur_row, cur_col):
+            if neighbor not in visited:
+                new_path = path.copy()
+                new_path.append(neighbor)
+                path_stack.append(new_path)
+
+    return path, num_states_explored
 
 
 def greedy(maze):
@@ -52,4 +92,45 @@ def greedy(maze):
 def astar(maze):
     # TODO: Write your code here
     # return path, num_states_explored
-    return [], 0
+    start = maze.getStart()
+    to_visit = queue.PriorityQueue()
+    to_visit.put((1, start, 0)) #(priority, (x,  y), g)
+    path_tracker = {start: None}
+    path = []
+    visited = []
+    num_states_explored = 0
+    end_state = (0, 0)
+
+    while not to_visit.empty():
+        curr_state = to_visit.get()
+
+        if curr_state[1] not in visited:
+
+            visited.append(curr_state[1])
+            num_states_explored += 1
+
+            if maze.isObjective(curr_state[1][0], curr_state[1][1]):
+                end_state = curr_state[1]
+                break
+
+            neighbors = maze.getNeighbors(curr_state[1][0], curr_state[1][1])
+            for neighbor in neighbors:
+                if neighbor not in visited and maze.isValidMove(neighbor[0], neighbor[1]):
+                    to_visit.put((manhattan_dist(neighbor, maze) + curr_state[2] + 1, neighbor, curr_state[2] + 1))
+                    path_tracker[neighbor] = curr_state[1]
+
+    while end_state:
+        path.insert(0, end_state)
+        end_state = path_tracker[end_state]
+
+    return path, num_states_explored
+
+def manhattan_dist(pos, maze):
+    objectives = maze.getObjectives()
+    min_heuristic = sys.maxsize
+    for objective in objectives:
+        heuristic = abs(pos[0] - objective[0]) + abs(pos[1] - objective[1])
+        if heuristic < min_heuristic:
+            min_heuristic = heuristic
+
+    return min_heuristic
