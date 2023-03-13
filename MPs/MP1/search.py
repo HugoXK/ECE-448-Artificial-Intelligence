48cf20c358c26846a5bcfd10da010c0704c5d696
HugoXK <ke.xu@zju.edu.cn>
2023-03-13 19:09:09 +0800

    Update Astar Algorithm

--- MPs/MP1/search.py
+++ MPs/MP1/search.py
@@ -134,40 +134,128 @@ def greedy(maze):
     return path, num_states_explored
 
 
-def astar(maze):
-    start = maze.getStart()
-    frontier = queue.PriorityQueue() # frontier, sorted queue, (Priority,Data)
-    frontier.put((cost(maze,start,[]), [start])) #(priority, path)
-    visited = set()# set of points that has been visited
-    while not frontier.empty():
-        #   current path & point
-        curr_path = frontier.get()[1]
-        curr_row, curr_col = curr_path[-1]
-        curr_point = (curr_row, curr_col)
-        #  if find the end
-        if maze.isObjective(curr_row, curr_col):
-            return curr_path, len(visited)
-        #  this point has been visited
-        if curr_point in visited:
-            continue
-        visited.add(curr_point)
-        #  add new point into new path
-        for new_point in maze.getNeighbors(curr_row, curr_col):
-            if new_point not in visited:
-                frontier.put((cost(maze,new_point,curr_path), curr_path + [new_point]))
-    return [], 0
 
-def h_dist(maze,point):
-    ends = maze.getObjectives()
+   
+class MST:
+    # minimum spanning tree for objectives, heuristic distance
+    def __init__(self, objectives):
+        self.dists = {(x, y): abs(x[0]-y[0])+abs(x[1]-y[1]) for x, y in self.pairs(objectives)}
+        self.union_set = {key: {key} for key in objectives} # len()!=0 for key of the set, 0 for others
+        self.roots = {key: key for key in objectives} # root for this node
+
+    # pairs for calculate dist, no repeat
+    def pairs(self, keys):
+        return (pairs for tuples in (((x,y) for x in keys if x < y) for y in keys) for pairs in tuples  )  
+        # pairs=set()
+        # for x in keys:
+        #     for y in keys:
+        #         if x < y:
+        #             pairs.add((x,y))
+        # return pairs
+    
+    # Kruskal's algorithm
+    def get_mst_cost(self):
+        cost = 0
+        # add edges with min dist
+        for dist, x, y in sorted((self.dists[(x, y)], x, y) for (x, y) in self.dists):
+            if self.union(x, y):
+                cost += dist
+        return cost
+
+    # check if x,y have the same root (same union set)
+    # if not connect them and add the distance
+    # else do nothing
+    def union(self, x, y):
+        # if x,y not in the same union set, add them togather
+        rx=self.roots[x]
+        ry=self.roots[y]
+        if rx == ry:
+            return False
+        else:
+            set_x=self.union_set[rx]
+            set_y=self.union_set[ry]
+            merge_set=set.union(set_y,set_x)
+            for key_y in self.union_set[y]:
+                self.roots[key_y]=rx
+                self.union_set[key_y]=merge_set
+            for key_x in self.union_set[x]:
+                self.roots[key_x]=rx
+                self.union_set[key_x]=merge_set
+            # self.union_set[rx]=set.union(set_y,set_x)
+            # self.union_set[ry]=set()
+            return True
+        
+# x,y -> dist (Manhattan distance)
+# def d(x,y):
+#     return abs(x[0]-y[0])+abs(x[1]-y[1])
+
+# heuristic distance, choose min for consistent heuristic
+def get_h_min(point,ends):
     h_min = sys.maxsize
     for end in ends:
-        h= abs(point[0] - end[0]) + abs(point[1] - end[1])
+        h= abs(point[0]-end[0])+abs(point[1]-end[1])
         if h < h_min:
             h_min = h
     return h_min
 
-def g_dist(path):
+# path -> dist (Manhattan distance)
+def g(path):
     return len(path)
-    
-def cost(maze,point,path):
-    return h_dist(maze,point)+g_dist(path)
+
+# get min cost for the multi points problem by using MST
+# path,maze -> cost, num_of_Objectives_not_visited, 
+def get_c_min(path,maze):
+    ends=maze.getObjectives() # Objectives as the end points of the path
+    not_visited=[] # Objectives not visited
+    for end in ends:
+        if end not in path:
+            not_visited.append(end)
+    # if all visited
+    if(len(not_visited)==0):
+        return 0,0,''
+    h_min = get_h_min(path[-1],not_visited)
+    tree=MST(not_visited)
+
+    return h_min+g(path)+tree.get_mst_cost(),len(not_visited),str(not_visited)
+
+def astar(maze):
+    start = maze.getStart()
+    frontier = queue.PriorityQueue() # frontier, sorted queue, (Priority,Data)
+    frontier.put((get_c_min([start],maze)[0], [[start]])) #(priority, path list)
+    visited = {} # dict of points that has been visited
+    num_of_states=0
+    while not frontier.empty():
+        #   current list of path, path & point
+        curr_list = frontier.get()[1]
+        curr_path = curr_list[0]
+        curr_row, curr_col = curr_path[-1]
+        curr_point = (curr_row, curr_col)
+        num_of_states += 1
+        
+        #  if find the end
+        if (get_c_min(curr_path,maze)[1]==0):
+            # num_of_states=len(visited)
+            # num_of_states=0
+            # for sets in visited:
+            #     num_of_states+=len(sets)
+            return curr_path, num_of_states
+        
+        #  this point has been visited
+        not_visited = get_c_min(curr_path,maze)[2]
+        if(not_visited in visited.keys()):
+            if curr_point in visited[not_visited]:
+                continue
+        else:
+            visited[not_visited]=[]
+        visited[not_visited].append(curr_point)
+        # print(not_visited,"\n",visited[not_visited],"\n\n")
+        # num+=len(visited[not_visited])
+        
+        #  add new point into new path
+        for new_point in maze.getNeighbors(curr_row, curr_col):
+            if new_point not in visited[not_visited]:
+                new_path=curr_path+[new_point]
+                frontier.put((get_c_min(new_path,maze)[0], [new_path]))
+    return [], 0
+
+
